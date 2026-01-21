dc637d4cda4d82a7c5f38c1deaaa324c9fe7fda5
CameronRhea <cameron.rhea@student.nmt.edu>
2026-01-21 14:16:32 -0700

    added oper codes

---
+++ codes.h
@@ -0,0 +1,43 @@
+
+
+
+// Operators
+#define SUBTRACTION              500
+#define MULTIPLICATION           501
+#define DIVISION                 502
+#define MODULUS                  503
+#define INCREMENT                504
+#define DECREMENT                505
+#define AUGM_ASSIGN_ADD          506
+#define AUGM_ASSIGN_SUB          507
+#define AUGM_ASSIGN_MULT         508
+#define AUGM_ASSIGN_DIV          509
+#define AUGM_ASSIGN_MOD          510
+#define EQUAL_TO                 511
+#define NOT_EQUAL_TO             512
+#define GREATER_THAN             513
+#define LESS_THAN                514
+#define GREATER_THAN_EQUAL       515
+#define LESS_THAN_EQUAL          516
+#define LOGICAL_AND              517
+#define LOGICAL_OR               518
+#define LOGICAL_NOT              519
+#define REF_EQUAL                520
+#define REF_NOT_EQUAL            521
+#define ACCESS_LEFT              522
+#define ACCESS_RIGHT             523
+#define ASSERT_NON_NULLABLE      524
+#define SAFE_CALL                525
+#define ELVIS                    526
+#define CREATE_REF               527
+#define CREATE_RANGE             528
+#define CREATE_RANGE_LESS        529
+#define SEPERATE_NAME            530
+#define NULLABLE                 531
+#define SEPARATES_LAMBDA         532
+#define ANNOTAION                533
+#define SEPARATES_STATEMENT      534
+#define REF_AS_STRING            535
+#define SUBSTITUTE_UNUSED_LAMBDA 536
+#define COMMA                    537
+#define DOT                      538
